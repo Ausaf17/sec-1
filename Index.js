3f7aa1a61623b89bc153572fc274a5ba9047fd12
Ausaf17 <kausaf98@gmail.com>
2024-09-24 17:29:33 +0530

    index.js

---
+++ Index.js
@@ -0,0 +1,27 @@
+console.log('hello javascript');
+
+a = 2234;
+console.log( typeof a );
+
+message = "be cool"
+console.log(message);
+console.log(typeof message);
+isHungry = true;
+console.log(isHungry);
+
+[x, y, z] = [2,3,4];
+console.log(x+y+z);
+
+
+// exponet power
+console.log(2**4);
+
+console.log( 1+1 );
+console.log( 1 +'1' );
+console.log( null+1 );
+console.log( 1 == 1 );
+console.log( 1 == '1' );
+console.log( 1 === '1' );
+
+
+
---
+++ condition.js
@@ -0,0 +1,26 @@
+n = 34;
+if(n%2 === 0){
+
+message = 'good';
+var message2 = 'nice';
+let message3 = 'cool';
+
+    console.log('even')
+}else{
+    console.log('odd')
+}
+console.log(message);
+console.log(message2);
+// console.log(message3);
+
+// wap to check if a number is divisible by both 7 and 11
+
+const m = 334;
+
+if( m % 7 === 0 && m % 11 === 0 ){
+    console.log( 'divisible by both 7 and 11');
+    
+}else{
+    console.log('not divisble by both 7 and 11' );
+    
+}
