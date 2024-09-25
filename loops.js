b4d6b604a09f77d2411ababa1dc19986ba77386a
Ausaf17 <kausaf98@gmail.com>
2024-09-25 17:36:51 +0530

    loops

--- loops.js
+++ loops.js
@@ -20,3 +20,48 @@
     b+=2
     
  }while(b<20);
+ console.log('----');
+ 
+
+
+ //print all even number from 100 to 200
+
+ for(let i=100; i<=200;i++){
+   if(i%2===0){ 
+      console.log(i);
+      
+   }
+ }
+
+ //wap to check a number is prime or not
+
+ let num = 10;
+ let prime = true;
+
+ for(let i=2; i<num; i++){
+if(num % i===0){
+   console.log('not prime');
+   prime = false;
+   break;
+   
+}
+
+
+ }
+ if(prime) console.log('prime');
+
+ //Wap to reverse the number
+  
+ let n1 =826357;
+ let reverse = 0;
+
+ while(n1<0){
+   console.log(n1, reverse);
+   const d = n1%10;
+   reverse = reverse*10+d;
+   n1= parseInt(n1/10);
+   
+ }
+ console.log('n1, reverse');
+ 
+ 
